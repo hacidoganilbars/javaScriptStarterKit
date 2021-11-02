5015308faf7696718a1771e4253bcae80f1cf750
Haci Dogan ilbars <doganilbars80@gmail.com>
2021-11-02 03:33:52 +0300

    2 Aralık Javascript Kodlarım

---
+++ src/functions.js
@@ -0,0 +1,21 @@
+function addToCart(quantity, productName="Kavun") {
+    console.log("Sepete eklendi: ürün: "+productName+" adet: "+quantity)
+}
+
+//addToCart()
+//addToCart("Elma")
+addToCart(10,"Yumurta")
+addToCart(12)
+//addToCart("Karpuz")
+
+let sayHello = ()=>{
+    console.log("Hello World!")
+}
+
+sayHello()
+
+let sayHello2= function () {
+    console.log("Hello World2!")
+}
+
+sayHello2()
